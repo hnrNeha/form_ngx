6510a3d56666926912313e0f05cecd9cbafa769f
hnrneha <hnrneha123@gmail.com>
2023-08-09 21:59:44 +0530

    this

--- server/config/config.js
+++ server/config/config.js
@@ -1,9 +1,10 @@
 module.exports = {
-  'database' : 'mongodb://localhost:27017/ngx_forms',
+  // 'database' : 'mongodb://localhost:27017/ngx_forms',
+  'database':'mongodb+srv://hnrneha123:<F9FKOgdRo6CCKB9Q>@form.ewv6agt.mongodb.net/',
   'secret'   : 'SUPERsecret', // change this to a hard to guess random string. it's for jwt encryption and decryption
-  'api_user' : 'YOUR_SENDGRID_USERNAME',
-  'api_key'  : 'YOUR_SENDGRID_PASSWORD',
-  'jwtExpire': '72h', //set the jwtExpire in smaller period in production
+  'api_user' : 'hnrneha123',
+  'api_key'  : 'F9FKOgdRo6CCKB9Q',
+  // 'jwtExpire': '72h', //set the jwtExpire in smaller period in production
   'paths'    : {
     // path for pets images
     serverPath      : '../server/app',
